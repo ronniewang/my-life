9394166ceae9f072e2b7ba8112e8305ce6040d9b
Ronnie Wang <ro_wsy@163.com>
2016-04-22 18:07:27 +0800

    sync sql file

--- sql/my-life.sql
+++ sql/my-life.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50711
 File Encoding         : 65001
 
-Date: 2016-04-22 14:35:18
+Date: 2016-04-22 18:07:07
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -59,7 +59,7 @@ CREATE TABLE `t_my_life_event` (
   `created_time` datetime NOT NULL,
   `last_modified_time` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of t_my_life_event
@@ -69,6 +69,8 @@ INSERT INTO `t_my_life_event` VALUES ('7', '2016-04-22 07:15:10', '2016-04-01 08
 INSERT INTO `t_my_life_event` VALUES ('9', '2016-04-01 08:10:10', '2016-04-22 12:00:42', '查找time-line控件并调试', 'http://www.helloweba.com/demo/v_timeline/', '0', '1', '1', '2016-04-22 13:39:40', '2016-04-22 13:39:43');
 INSERT INTO `t_my_life_event` VALUES ('10', '2016-04-22 12:00:42', '2016-04-22 13:30:03', '吃东方宫炒面片，午休 ', null, '0', '1', '1', '2016-04-22 13:40:39', '2016-04-22 13:40:42');
 INSERT INTO `t_my_life_event` VALUES ('11', '2016-04-22 13:30:03', '2016-04-22 14:34:03', '为spring-boot的tomcat配置https支持', 'https://drissamri.be/blog/java/enable-https-in-spring-boot/', '0', '1', '1', '2016-04-22 14:35:08', '2016-04-22 14:35:10');
+INSERT INTO `t_my_life_event` VALUES ('12', '2016-04-22 14:34:03', '2016-04-22 17:00:01', '调试unique_id的bug，由于把原始引用id给改了，所以比赛时间就错了，unique_id生成并无问题', null, '0', '1', '1', '2016-04-22 17:00:39', '2016-04-22 17:00:42');
+INSERT INTO `t_my_life_event` VALUES ('13', '2016-04-22 17:00:01', '2016-04-22 18:06:13', '模仿github代码比例条，生成时间线', 'https://github.com/ronniewang/my-life', '0', '1', '1', '2016-04-22 18:06:53', '2016-04-22 18:06:55');
 
 -- ----------------------------
 -- Table structure for t_user
